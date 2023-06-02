e4f7e24215b319cf5d140a9c6401447065d51827
tower yang <jkjd.yangchongxiao@aisino.com>
2023-06-02 10:22:27 +0800

    BeanUtils

---
+++ src/main/java/cn/bbahh/util/bean/BeanUtils.java
@@ -0,0 +1,31 @@
+package cn.bbahh.util.bean;
+
+import java.lang.reflect.Field;
+import java.util.HashMap;
+import java.util.Map;
+
+public class BeanUtils {
+    public static Map<String,Object> beanToMap(Object object) throws IllegalAccessException {
+        Map<String, Object> map = new HashMap<>(1);
+        Field[] fields = object.getClass().getDeclaredFields();
+        for (Field field : fields) {
+            field.setAccessible(true);
+            map.put(field.getName(), field.get(object));
+        }
+        return map;
+    }
+
+    public static <T> T mapToBean(Map<String,Object> source,Class<T> target) throws Exception {
+        Field[] fields = target.getDeclaredFields();
+        T o = target.newInstance();
+        for(Field field:fields){
+            Object val;
+            if((val=source.get(field.getName()))!=null){
+                field.setAccessible(true);
+                field.set(o,val);
+            }
+        }
+        return o;
+    }
+
+}
