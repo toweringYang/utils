81924c13ddd17c95ce7b2a6c208d9cb2758b59a0
tower yang <jkjd.yangchongxiao@aisino.com>
2023-06-21 08:54:34 +0800

    工具维护

--- src/main/java/cn/bbahh/util/StrUtil.java
+++ src/main/java/cn/bbahh/util/StrUtil.java
@@ -1,4 +1,13 @@
 package cn.bbahh.util;
 
 public class StrUtil {
+
+    public static boolean isEmpty(String str){
+        return str==null || str.length()==0;
+    }
+
+    public static boolean isNotEmpty(String str){
+        return !isEmpty(str);
+    }
+
 }
