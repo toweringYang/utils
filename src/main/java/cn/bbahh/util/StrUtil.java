55a972819adb9a4ac0a4501ddd0f5297b7918369
tower yang <jkjd.yangchongxiao@aisino.com>
2023-09-04 13:17:23 +0800

    word 转pdf

--- src/main/java/cn/bbahh/util/StrUtil.java
+++ src/main/java/cn/bbahh/util/StrUtil.java
@@ -10,4 +10,21 @@ public class StrUtil {
         return !isEmpty(str);
     }
 
+    public static boolean containsChineseCharacters(String str) {
+        if (str == null) {
+            return false;
+        }
+        return str.chars().anyMatch(c -> isChineseCharacter((char) c));
+    }
+    public static boolean isChineseCharacter(char c) {
+        Character.UnicodeBlock ub = Character.UnicodeBlock.of(c);
+        return ub == Character.UnicodeBlock.CJK_UNIFIED_IDEOGRAPHS
+                || ub == Character.UnicodeBlock.CJK_COMPATIBILITY_IDEOGRAPHS
+                || ub == Character.UnicodeBlock.CJK_UNIFIED_IDEOGRAPHS_EXTENSION_A
+                || ub == Character.UnicodeBlock.CJK_UNIFIED_IDEOGRAPHS_EXTENSION_B
+                || ub == Character.UnicodeBlock.CJK_SYMBOLS_AND_PUNCTUATION
+                || ub == Character.UnicodeBlock.HALFWIDTH_AND_FULLWIDTH_FORMS
+                || ub == Character.UnicodeBlock.GENERAL_PUNCTUATION;
+    }
+
 }
